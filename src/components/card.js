9ac3b5a981839e557de8fd41c43e61ad3e08ae23
jlcampbell16 <45213854+Jlcampbell16@users.noreply.github.com>
2019-04-25 21:49:39 -0600

    cards render and shuffle, score and top score counters working, basic css

--- src/components/card.js
+++ src/components/card.js
@@ -1,18 +1,21 @@
 import React from "react";
+import "../App.css";
+// const styles = {
+//   .card: {
+//     margin: 20,
+//     height: 100,
+//     width: 100,
+//     objectFit: "cover"
+//   }
+// };
 
-const styles = {
-  card: {
-    margin: 20,
-    height: 100,
-    width: 100,
-    objectFit: "cover"
-  }
-};
-
-function Card() {
+function Card(props) {
   return (
-    <>
-    </>
+      <div className="card" >
+        <div className="img-container">
+          <img className="cardImg" alt={props.name} src={props.image} onClick={() => props.clickedCard(props.id)} />
+        </div>
+      </div>
   );
 }
 
--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "../App.css";
 
 function Jumbotron() {
   return (
-    <div>
-      <h1>Clicky Game!</h1>
-      <h3>Test your memory by clicking the cards but not clicking the same character more than once.</h3>
+    <div className="jumbotron">
+      {/* <h1>Clicky Game!</h1>
+      <h3>Test your memory by clicking the cards but not clicking the same character more than once.</h3> */}
     </div>
   );
 }
